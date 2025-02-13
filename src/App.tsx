b87ab475b32cca9f88fce7f53ca555539f00f10b
wxfeiang <wxfeiang@qq.com>
2025-02-13 23:00:51 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="p-10 ">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { presetAttributify, presetUno } from 'unocss'
+import Unocss from 'unocss/vite' // 关键插件引入
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    Unocss({
+      presets: [presetUno(), presetAttributify()],
+      // 也可以指定配置文件（如果使用独立配置）
+      configFile: 'unocss.config.ts'
+    })
+  ]
 })
